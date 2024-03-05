f9962fbe5042283b227b4be3ab37fe83944f74e9
terrahsa@gmail.com <118025389+AdrianB378@users.noreply.github.com>
2024-03-05 11:23:20 -0300

    se ha subido proyecto

---
+++ src/main/java/com/todocodeacademy/pruebaspringcloud/PruebaspringcloudApplication.java
@@ -0,0 +1,13 @@
+package com.todocodeacademy.pruebaspringcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaspringcloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaspringcloudApplication.class, args);
+	}
+
+}
